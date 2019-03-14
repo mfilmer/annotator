5a96b29c81dc95154c4fda85c3ec0cc4a00bb9cf
Matthew Filmer <mfilmer@nd.edu>
2019-03-14 19:36:52 -0400

    Window resizing basically working

--- application.m
+++ application.m
@@ -17,44 +17,47 @@ classdef application < handle
     end
     methods
         function this = application()
-            % Create UI
+            % Set window dimensions
             X = 400;
             Y = 100;
             width = 1024;
             height = 760;
-            this.window = figure('Position', [X, Y, width, height]);
+            
+            % Create UI Elements
+            this.window = figure('Position', [X, Y, width, height], 'ResizeFcn', @(src,eventdata) this.redrawWindow());
             this.fileList = fileList(this.window);
             this.tabGroup = uitabgroup(this.window, 'Units', 'pixels');
             this.tabs = uitab(this.tabGroup, 'Title', 'aoeu');
             this.buttonPanel = uipanel(this.window, 'Units', 'pixels');
             uicontrol('Parent', this.buttonPanel, 'Style', 'pushbutton', 'String', 'aoeu');
             
+            % Position UI elements
             this.redrawWindow();
         end
         
         function redrawWindow(this)
-            % Get window location and dimensions
-            pos = get(this.window, 'Position');
-            X = pos(1);
-            Y = pos(2);
-            width = pos(3);
-            height = pos(4);
-            
-            % Enforce minimum window size
-            if width < this.minWidth
-                width = this.minWidth;
+            if(~isempty(this.window))
+                % Get window location and dimensions
+                pos = get(this.window, 'Position');
+                X = pos(1);
+                Y = pos(2);
+                width = pos(3);
+                height = pos(4);
+                
+                % Enforce minimum window size
+                if width < this.minWidth
+                    width = this.minWidth;
+                end
+                if height < this.minHeight
+                    height = this.minHeight;
+                end
+                
+                % Move elements
+                %set(this.window, 'Position', [X, Y, width, height]);
+                this.fileList.setPosition([0, 0, this.fileListWidth, height]);
+                set(this.tabGroup, 'Position', [this.fileListWidth, 0, width-this.fileListWidth-this.buttonPanelWidth, height]);
+                set(this.buttonPanel, 'Position', [width-this.buttonPanelWidth,0,this.buttonPanelWidth, height]);
             end
-            if height < this.minHeight
-                height = this.minHeight;
-            end
-            set(this.window, 'Position', [X, Y, width, height]);
-            
-            % Move elements
-            set(this.window, 'Position', [X, Y, width, height]);
-            this.fileList.setPosition([0, 0, this.fileListWidth, height]);
-            set(this.tabGroup, 'Position', [this.fileListWidth, 0, width-this.fileListWidth-this.buttonPanelWidth, height]);
-            set(this.buttonPanel, 'Position', [width-this.buttonPanelWidth,0,this.buttonPanelWidth, height]);
-            
         end
         
         function h = newButton(this, parent, pos, label)
